22a74922e5de991a052d50ce34203f585554f33f
brittonashford <brittonashford@users.noreply.github.com>
2022-01-06 22:37:51 -0600

    add new routing, forms, redirects, pug templates, and cookies.

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+
 const app = express();  //returns an express application
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookiePareser());
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-	res.render('index'); //refers to the index.pug file. extension not needed.
+	const name = req.cookies.username;
+	if (name) {
+		res.render('index', { name });
+	} else {
+		res.redirect('/hello');
+	}
+});
+
+app.get('/cards', (req, res) => {
+	res.render('cards', {
+		prompt: "Who is buried in Grant's tomb?",
+		colors
+	});
 });
 
 app.get('/hello', (req, res) => {
-	res.send("<h1>Hello! Here is another page</h1>")
-});
+	const name = req.cookies.username;
+	if (name) {
+		res.redirect('/');
+	} else {
+		res.render('hello');
+	}	
+})
+
+app.post('/hello', (req, res) => {
+	res.cookie('username', req.body.username)
+	res.redirect('/');
+})
+
+app.post('/goodbye', (req, res) => {
+	res.clearCookie('username');
+	res.redirect('/hello');
+})
 
 app.listen(3001, () => {
 	console.log('the application is running on localhost:3001');
